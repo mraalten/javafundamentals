ec852a100d624e06861893f9b1d86fb5eb8d0ab0
raalten <raalten@bol.com>
2018-10-02 10:46:39 +0200

    Code examples in separate project

--- java-backend/src/main/java/nl/craftsmen/javafundamentals/technical/MyController.java
+++ java-backend/src/main/java/nl/craftsmen/javafundamentals/technical/MyController.java
@@ -1,4 +1,4 @@
-package nl.craftsmen.javafundamentals.backend;
+package nl.craftsmen.javafundamentals.technical;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
