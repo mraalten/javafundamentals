0ba7842619617b7b1fe055861c2d07ae9096bba4
raalten <raalten@bol.com>
2018-10-09 14:55:42 +0200

    moved

--- java-backend/src/main/java/nl/craftsmen/javafundamentals/technical/BankApplication.java
+++ java-backend/src/main/java/nl/craftsmen/javafundamentals/technical/BankApplication.java
@@ -1,4 +1,4 @@
-package nl.craftsmen.javafundamentals.backend;
+package nl.craftsmen.javafundamentals.technical;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
